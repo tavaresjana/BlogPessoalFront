af7b1ee903106ef09bbef38201d85ce6e7be25b7
Janaina Tavares <tjanaina92@gmail.com>
2022-06-01 20:15:19 -0300

    Rotas de entrar e cadastrar criadas

---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,9 @@
+export class User {
+    public id: number;
+    public name: string;
+    public usuario: string;
+    public senha: string;
+    public foto: string;
+    public tipo: string;
+    private token: string;
+}
