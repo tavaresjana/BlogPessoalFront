7c19f21fc3968e0e21a146516aec9efe4402110f
Janaina Tavares <tjanaina92@gmail.com>
2022-06-08 21:53:23 -0300

    Implementação  do post e get de postagens

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -20,6 +20,11 @@ export class AuthService {
   
   cadastrar(user: User): Observable<User> {
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
+  }
+
+  getByIdUser(id: number): Observable<User> {
+    return this.http.get<User>(`http://localhost:8080/usuarios/${id}`, 
+    {headers: new HttpHeaders().set('Authorization', environment.token)})
   }
 
   logado(){
